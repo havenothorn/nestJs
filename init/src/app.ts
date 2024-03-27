16e8607a28b3772204a125a872fd0ddc8614be5d
Nami <havenothorn@gmail.com>
2024-03-27 22:28:38 +0900

    add: middleware

--- init/src/app.ts
+++ init/src/app.ts
@@ -3,11 +3,32 @@ import { Cat, CatType } from "./app.model";
 
 const app: express.Express = express();
 
+app.use(
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    console.log(req.rawHeaders[1]);
+    console.log("this is a logging middleware function. :)");
+    next();
+  }
+);
+
 app.get("/", (req: express.Request, res: express.Response) => {
-  console.log(req);
   res.send({ cats: Cat });
+});
+
+app.get("/cats/blue", (req: express.Request, res: express.Response) => {
+  res.send({ blue: Cat[0] });
+});
+
+app.get("/cats/som", (req: express.Request, res: express.Response) => {
+  res.send({ som: Cat[1] });
 });
 
 app.listen(8000, () => {
   console.log("Server is running on port 8000");
 });
+
+app.use(
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.send({ error: "404 Not Found" });
+  }
+);
