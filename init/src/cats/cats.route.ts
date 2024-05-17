e3078ce6da8e456d36d4514c9db6aaa4251ccddd
havenothorn <havenothorn@gmail.com>
2024-05-17 09:04:41 +0900

    add: UPDATE

--- init/src/cats/cats.route.ts
+++ init/src/cats/cats.route.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 
 const router = Router();
 
-// Read 고양이 전체 데이터 조회
+// READ 고양이 전체 데이터 조회
 router.get("/cats", (req, res) => {
   try {
     const cats = Cat;
@@ -22,7 +22,7 @@ router.get("/cats", (req, res) => {
   }
 });
 
-// Read 특정 고양이 데이터 조회
+// READ 특정 고양이 데이터 조회
 router.get("/cats/:id", (req, res) => {
   try {
     const params = req.params;
@@ -44,7 +44,7 @@ router.get("/cats/:id", (req, res) => {
   }
 });
 
-// Create 새로운 고양이 추가 api
+// CREATE 새로운 고양이 추가 api
 router.post("/cats", (req, res) => {
   try {
     const data = req.body;
@@ -61,5 +61,33 @@ router.post("/cats", (req, res) => {
     });
   }
 });
+
+// UPDATE 고양이 데이터 업데이트 => PUT
+router.put("/cats/:id", (req, res) => {
+  try {
+    const params = req.params;
+    const body = req.body;
+    let result;
+    Cat.forEach((cat) => {
+      if (cat.id === params.id) {
+        cat = body;
+        result = cat;
+      }
+    });
+    res.status(200).send({
+      success: true,
+      data: { cat: result },
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+// UPDATE 고양이 데이터 일부 업데이트 => PATCH
+
+// DELETE 고양이 데이터 삭제 => DELETE
 
 export default router;
